0fd361caeccfb49cd4f0ca262341c8ec17381371
Michael Dillie <michaeldillie@gmail.com>
2018-05-01 08:29:03 -0500

    Setting up firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCxHN0ybdLlKv5yJMs7JbiKEdIGYi6urRU",
+    authDomain: "trainschedule-c455f.firebaseapp.com",
+    databaseURL: "https://trainschedule-c455f.firebaseio.com",
+    projectId: "trainschedule-c455f",
+    storageBucket: "",
+    messagingSenderId: "906362364071"
+  };
+  firebase.initializeApp(config);
+  
 });
